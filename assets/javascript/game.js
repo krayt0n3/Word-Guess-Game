b97ef25829160cf2d4e5ac0fd7a927808d634d91
Krayt0n3 <hendersonkevin64@gmail.com>
2019-03-15 12:39:49 -0500

    centered text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,13 @@
 var words = ['turtle', 'elephant', 'ostrich', 'bull', 'dog', 'frog'];
-var remainingGuess = 8;
+var remainingGuess = 10;
 document.onkeyup = function(event) {
-    
+var each = Array.from(words[0]);
     var userGuess = event.key;
-
-    if ((userGuess === 't') || (userGuess === 'u') || (userGuess === 'r') || (userGuess === 'l') 
-    || (userGuess === 'e') || (userGuess === 'p') || (userGuess === 'h') || 
-    (userGuess === 'a') || (userGuess === 'n') || (userGuess === 'o') || (userGuess === 's') || 
-    (userGuess === 'i') || (userGuess === 'c') || (userGuess === 'b') || (userGuess === 'd') || 
-    (userGuess === 'g') || (userGuess === 'f')) {
-        alert('User Guess: ' + userGuess);
-    } else {
-        document.querySelector('.loss').innerHTML = remainingGuess--;
+    var collect = document.querySelector('.guesses').innerHTML = userGuess;
+    for (var i = 0; i < each.length; i++) {
+        if(userGuess === each[i]) {
+            document.querySelector('.correct').innerHTML = collect;
+        }else{ document.querySelector('.loss').innerHTML = remainingGuess--;
     }
+}
 }
