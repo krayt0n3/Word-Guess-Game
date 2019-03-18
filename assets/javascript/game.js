cebaccdc89d454cfbda89431b3d7d3443e7afbff
Krayt0n3 <hendersonkevin64@gmail.com>
2019-03-17 20:29:59 -0500

    redo the code, can count and track

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,22 @@
 var words = ['turtle', 'elephant', 'ostrich', 'bull', 'dog', 'frog'];
-var remainingGuess = 10;
-document.onkeyup = function(event) {
-var each = Array.from(words[0]);
+
+document.onkeyup = trackKeys;
+var count = 10;
+var lettersGuessed = [];
+function trackKeys(event) {
+    
     var userGuess = event.key;
-    var collect = document.querySelector('.guesses').innerHTML = userGuess;
-    for (var i = 0; i < each.length; i++) {
-        if(userGuess === each[i]) {
-            document.querySelector('.correct').innerHTML = collect;
-        }else{ document.querySelector('.loss').innerHTML = remainingGuess--;
+    if (count >= 1) {
+    count -= 1;
+    document.querySelector('.loss').innerHTML = count;
+    lettersGuessed.push(userGuess);
+    } else {
+        count = 10;
     }
-}
-}
+    
+    document.querySelector('.guesses').innerHTML = lettersGuessed;
+
+    
+};
+
+
